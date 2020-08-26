532a2e20a9c7b6289fee34a5397067ea45d0bdf0
Shreyash-Mall <67573963+Shreyash-Mall@users.noreply.github.com>
2020-08-26 13:18:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+	Expected output:
+	
+	Hello Yaakov
+	Good Bye John
+	Good Bye Jen
+	Good Bye Jason
+	Hello Paul
+	Hello Frank
+	Hello Larry
+	Hello Paula
+	Hello Laura
+	Good Bye Jim
+	*/
+	
+	(function() {
+	    var name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	    for (var i = 0; i < name.length; i++) {
+	        var first = name[i].charAt(0).toLowerCase();
+	        if (first === 'j') {
+	           console.log(" Good Bye "+ name[i]);
+	        } else {
+	            console.log(" Hello "+ name[i]);
+	        }
+	    }
+	})();
